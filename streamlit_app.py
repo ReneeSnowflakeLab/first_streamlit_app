a51e139da2f435df2f83af41ad248f18f3c935e1
ReneeSnowflakeLab <139887612+ReneeSnowflakeLab@users.noreply.github.com>
2023-07-18 10:54:40 -0400

    Update streamlit_app.py
    fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit
+import pandas
+
+my_fruit_list=pandas.read.csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe = (my_fruit_list)
+
 
 streamlist.title('My Parents New Healthy Diner')
 
