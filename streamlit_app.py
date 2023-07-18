5ee1b9d0019b959f6d35f03a329639e7f40026b4
ReneeSnowflakeLab <139887612+ReneeSnowflakeLab@users.noreply.github.com>
2023-07-18 10:58:53 -0400

    Update streamlit_app.py
    fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,12 @@ import pandas
 my_fruit_list=pandas.read.csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe = (my_fruit_list)
 
+#Let's put a pick list here
+streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index))
+
+#Display table on page
+streamlit.dataframe(my_fruit_list)
+
 
 streamlist.title('My Parents New Healthy Diner')
 
